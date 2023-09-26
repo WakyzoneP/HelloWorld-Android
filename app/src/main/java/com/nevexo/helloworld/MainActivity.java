dc77a4eb317cbe19cf16695b89b073b859106ddd
Vlad Beraru <vlad.beraru@nevexo.ro>
2023-09-26 18:12:28 +0300

    Last Commit

--- app/src/main/java/com/nevexo/helloworld/MainActivity.java
+++ app/src/main/java/com/nevexo/helloworld/MainActivity.java
@@ -3,13 +3,29 @@ package com.nevexo.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextName);
+
+    }
+
+    public void onBtnClick(View view) {
+        TextView textViewHello = findViewById(R.id.textViewHello);
+        String text = editText.getText().toString();
+        textViewHello.setText("Hello " + text);
     }
 
 }
